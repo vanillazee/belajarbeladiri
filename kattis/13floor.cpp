327598809f8b84c537feffde08fb4a96a5439279
unknown <zaidanpurbayu@gmail.com>
2026-01-31 00:00:53 +0700

    13

---
+++ kattis/13floor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int f;
+    cin >> f;
+
+    if(f<13) {
+        cout << f;
+    } else {
+        cout << f+1;
+    }
+
+    return 0;
+}
